10bb7338ec31aad9d87785cbf77c6438813cd9bd
NCJ Charlie <me@ncj.wiki>
2021-06-15 22:58:20 +0800

    Update department.js

--- src/appointments/department.js
+++ src/appointments/department.js
@@ -1,129 +1,86 @@
-import {Platform, StyleSheet, View} from 'react-native';
-
-
+import {StyleSheet, View} from 'react-native';
 import * as React from 'react';
-import {Divider, List} from 'react-native-paper';
+import {Appbar, List} from 'react-native-paper';
+import {useNavigation, useRoute} from "@react-navigation/native";
+import ScreenWrapper from "../components/ScreenWrapper";
 
 
-//样式
 const style = StyleSheet.create({
-
     container: {
-        // flexDirection: '',   // 水平排布
         flex: 1,
-        borderRadius: 5,  // 设置圆角边
-        backgroundColor: 'white',
-        paddingLeft: 10,
-        paddingRight: 10,
-        width: '95%',
-        resizeMode: 'stretch',
-        paddingTop: Platform.OS === 'ios' ? 20 : 0,  // 处理iOS状态栏
-        height: Platform.OS === 'ios' ? 88 : 48,   // 处理iOS状态栏
-        // 使元素垂直居中排布, 当flexDirection为column时, 为水平居中
     },
-    searchBox: {//搜索框
-        height: 30,
-        flexDirection: 'row',   // 水平排布
-        flex: 1,
-        borderRadius: 5,  // 设置圆角边
-        backgroundColor: 'white',
-        alignItems: 'center',
-        marginLeft: 8,
-        marginRight: 8,
-    },
-    searchIcon: {//搜索图标
-        height: 20,
-        width: 20,
-        marginLeft: 5,
-        marginRight: 8,
-        resizeMode: 'stretch'
-    },
-    inputText: {
-        flex: 1,
-        backgroundColor: 'transparent',
-        fontSize: 15,
-    },
-    voiceIcon: {
-        marginLeft: 5,
-        marginRight: 8,
-        width: 15,
-        height: 20,
-        resizeMode: 'stretch'
-    },
-    scanIcon: {//搜索图标
-        height: 26.7,
-        width: 26.7,
-        resizeMode: 'stretch'
-    },
-    listCard: {
-        alignItems: 'center',
-        padding: 30,
-        width: '100%'
-        // marginLeft: 8,
-        // marginRight: 8,
-    }
 });
 
-const MyList = () => {
-    const [expanded, setExpanded] = React.useState(true);
-    const handlePress = () => setExpanded(!expanded);
+const mockDepartmentsData = [
+    {
+        name: '门诊部',
+        departments: [
+            '呼吸科',
+            '心内科',
+            '消化科',
+            '外科',
+            '儿科',
+            '妇科',
+            '眼科',
+            '耳鼻喉科'
+        ]
+    },
+    {
+        name: '急诊室',
+        departments: [
+            '发烧急诊',
+            '吃坏肚子急诊',
+            '诺如病毒急诊'
+        ]
+    },
+    {
+        name: '住院部',
+        departments: [
+            '内科住院部',
+            '外科住院部',
+            '妇产科住院部',
+            '儿科住院部',
+            '监护室'
+        ]
+    },
+    {
+        name: '放射科',
+        departments: [
+            '我也不知道放射科里有啥'
+        ]
+    }
+]
+
+const DepartmentList = () => {
+    const navigation = useNavigation();
+    const route = useRoute();
 
     return (
-        <View>
-            <List.Section>
-                <View>
-                    <List.Accordion
-                        style={style.container}
-                        title="门诊部">
-                        <Divider/>
-                        <List.Item title="呼吸科"/>
-                        <List.Item title="心内科"/>
-                        <List.Item title="消化科"/>
-                        <List.Item title="外科"/>
-                        <List.Item title="儿科"/>
-                        <List.Item title="妇科"/>
-                        <List.Item title="眼科"/>
-                        <List.Item title="耳鼻喉科"/>
-                    </List.Accordion>
-                    <Divider/>
-                </View>
-
-                <List.Accordion
-                    title="急诊室"
-                    style={style.container}
-                    expanded={expanded}
-                    onPress={handlePress}>
-                    <Divider/>
-
-                </List.Accordion>
-
-                <List.Accordion
-                    title="住院部"
-                    style={style.container}
-                    expanded={expanded}
-                    onPress={handlePress}>
-                    <Divider/>
-                    <List.Item title="内科住院部"/>
-                    <List.Item title="外科住院部"/>
-                    <List.Item title="妇产科住院部"/>
-                    <List.Item title="儿科住院部"/>
-                    <List.Item title="监护室"/>
-                </List.Accordion>
-
-                <List.Accordion
-                    title="放射科"
-                    style={style.container}
-                    expanded={expanded}
-                    onPress={handlePress}>
-                    <Divider/>
-
-                </List.Accordion>
-            </List.Section>
+        <View style={style.container}>
+            <Appbar.Header>
+                <Appbar.BackAction onPress={() => {
+                    navigation.goBack()
+                }}/>
+                <Appbar.Content title="选择科室" subtitle={route.params.hospital.name}/>
+            </Appbar.Header>
+            <ScreenWrapper>
+                <List.Section title="请选择部门下属的科室">
+                    {mockDepartmentsData.map(({name, departments}) => {
+                        return <List.Accordion
+                            title={name}
+                            left={props => <List.Icon {...props} icon="hospital-building"/>}>
+                            {departments.map((department) => {
+                                return <List.Item title={department}/>
+                            })}
+                        </List.Accordion>
+                    })}
+                </List.Section>
+            </ScreenWrapper>
         </View>
     );
 };
 
-export default MyList;
+export default DepartmentList;
 
 
 
