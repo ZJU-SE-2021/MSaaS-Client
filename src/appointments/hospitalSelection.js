282827ec225396efe8162c68299c0333abc9668a
NCJ Charlie <me@ncj.wiki>
2021-06-20 01:30:55 +0800

    add loading placeholder

--- src/appointments/hospitalSelection.js
+++ src/appointments/hospitalSelection.js
@@ -1,8 +1,9 @@
 import {Searchbar, List, FAB, Card} from 'react-native-paper';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {View, StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import ScreenWrapper from "../components/ScreenWrapper";
+import LoadingWrapper from "../components/LoadingWrapper";
 
 
 class Hospital {
@@ -11,6 +12,14 @@ class Hospital {
         this.distance = distance;
     }
 }
+
+const hospitals = [
+    new Hospital('杭州市第一医院', '1.2'),
+    new Hospital('杭州市第二医院', '1.3'),
+    new Hospital('浙大医学院附属第一医院', '1.3'),
+    new Hospital('浙大医学院附属第二医院', '1.4'),
+    new Hospital('浙大医学院附属第三医院', '2.8')
+]
 
 const styles = StyleSheet.create({
     view: {
@@ -42,38 +51,40 @@ export default function HospitalSelection() {
     const navigation = useNavigation();
     const [searchQuery, setSearchQuery] = useState('');
     const [cardOrList, setCardOrList] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const onChangeSearch = query => setSearchQuery(query);
 
-    const hospitals = [
-        new Hospital('杭州市第一医院', '1.2'),
-        new Hospital('杭州市第二医院', '1.3'),
-        new Hospital('浙大医学院附属第一医院', '1.3'),
-        new Hospital('浙大医学院附属第二医院', '1.4'),
-        new Hospital('浙大医学院附属第三医院', '2.8')
-    ];
+    useEffect(() => {
+        if (isLoading) {
+            const timeoutId = setTimeout(() => setIsLoading(false), 500);
+            return () => clearTimeout(timeoutId);
+        }
+    }, [])
 
     return (
         <View style={styles.view}>
             <Searchbar placeholder='搜索医院' onChangeText={onChangeSearch} value={searchQuery}/>
-            <ScreenWrapper>
-                <View style={!cardOrList ? styles.scrollView : ''}>
-                    {hospitals.filter((hospital) => {
-                        return (hospital.name.includes(searchQuery))
-                    })
-                        .map((hospital, index) => {
-                            return cardOrList ? (
-                                <List.Item key={index} title={hospital.name} description={hospital.distance + ' km'}
-                                           onPress={() => navigation.navigate('Hospital', {hospital})}/>
-                            ) : (
-                                <Card style={styles.card} key={index}
-                                      onPress={() => navigation.navigate('Hospital', {hospital})}>
-                                    <Card.Title title={hospital.name} subtitle={hospital.distance + ' km'}/>
-                                    <Card.Cover source={picPlaceHolder}/>
-                                </Card>
-                            )
-                        })}
-                </View>
-            </ScreenWrapper>
+            <LoadingWrapper isLoading={isLoading}>
+                <ScreenWrapper>
+                    <View style={!cardOrList ? styles.scrollView : ''}>
+                        {hospitals.filter((hospital) => {
+                            return (hospital.name.includes(searchQuery))
+                        })
+                            .map((hospital, index) => {
+                                return cardOrList ? (
+                                    <List.Item key={index} title={hospital.name} description={hospital.distance + ' km'}
+                                               onPress={() => navigation.navigate('Hospital', {hospital})}/>
+                                ) : (
+                                    <Card style={styles.card} key={index}
+                                          onPress={() => navigation.navigate('Hospital', {hospital})}>
+                                        <Card.Title title={hospital.name} subtitle={hospital.distance + ' km'}/>
+                                        <Card.Cover source={picPlaceHolder}/>
+                                    </Card>
+                                )
+                            })}
+                    </View>
+                </ScreenWrapper>
+            </LoadingWrapper>
             <FAB
                 style={styles.fab}
                 icon={cardOrList ? 'grid' : 'text'}
---
+++ src/components/LoadingWrapper.js
@@ -0,0 +1,23 @@
+import React from "react";
+import SkeletonContent from "react-native-skeleton-content";
+
+export default function LoadingWrapper({children, isLoading = true, style, ...rest}: Props) {
+    const placeHolderLayout = Array(6).fill(
+        {
+            width: "100%",
+            height: 80,
+            marginTop: 5,
+            marginBottom: 5,
+        }
+    )
+
+    return <SkeletonContent
+        containerStyle={{...style, flex: 1}}
+        isLoading={isLoading}
+        layout={placeHolderLayout}
+        highlightColor='#F5F5F5'
+        boneColor='#FFFFFF'
+    >
+        {children}
+    </SkeletonContent>
+}
